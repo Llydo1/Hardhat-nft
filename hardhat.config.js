2def477263050e0dd3c9daef1f715d0154e73876
Llydo1 <beaniss1357@gmail.com>
2022-12-11 20:00:40 +0700

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,56 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const RPC_URL_GOERLI = process.env.RPC_URL_GOERLI || 0;
+const KEY_GOERLI = process.env.KEY_GOERLI || 0;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 0;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 0;
+const RPC_URL_MAINET = process.env.RPC_URL_MAINET;
+
+module.exports = {
+    solidity: {
+        compilers: [
+            { version: "0.8.7" },
+            { version: "0.6.12" },
+            { version: "0.6.6" },
+            { version: "0.4.19" },
+        ],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: RPC_URL_GOERLI,
+            accounts: [KEY_GOERLI],
+            chainId: 5,
+            blockConfirmation: 6,
+        },
+        hardhat: {
+            chainId: 31337,
+            blockConfirmation: 1,
+            forking: {
+                url: RPC_URL_MAINET,
+            },
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        currency: "USD",
+    },
+};
